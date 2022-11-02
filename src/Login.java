8f6db63bc618671731ebff69276e2b7f1fc9d4ac
Bartłomiej Wydrzycki <92253080+Wydreq@users.noreply.github.com>
2022-11-02 18:34:36 +0100

    Fixed login bug

--- src/Login.java
+++ src/Login.java
@@ -1,6 +1,7 @@
 import javax.swing.*;
 import java.awt.*;
 import java.sql.*;
+import java.awt.event.*;
 
 public class Login extends JFrame {
     void login(final JFrame frame) {
@@ -19,5 +20,26 @@ public class Login extends JFrame {
         submitButton.setBounds(150,350,300,40 );
         frame.add(l1); frame.add(l2); frame.add(l3); frame.add(usernameInput); frame.add(passwordInput); frame.add(submitButton);
         frame.repaint();
+
+        submitButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent ae) {
+
+                try {
+                    Class.forName("oracle.jdbc.driver.OracleDriver");
+                    Connection conn =DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe","system","sys");
+                    String sql = "SELECT * FROM USERS WHERE LOGIN='"+usernameInput.getText()+"' AND PASSWORD='"+passwordInput.getText()+"'";
+                    PreparedStatement ps = conn.prepareStatement(sql);
+                    ResultSet rs=ps.executeQuery();
+                    if(rs.next()) {
+                        JOptionPane.showMessageDialog(null, "Login successfully!");
+                    }
+                    else {
+                        JOptionPane.showMessageDialog(null, "Something went wrong :(");
+                    }
+                }catch(Exception e) {System.out.println(e);}
+            }
+        });
+
     }
 }
